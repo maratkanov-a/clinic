530a3c6d198fef34aed8477d51f77f288337dfe1
maratkanov-a <a.s.maratkanov@gmail.com>
2016-04-05 09:51:04 +0300

    added media root and media url

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from clinic import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('clinic_app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
