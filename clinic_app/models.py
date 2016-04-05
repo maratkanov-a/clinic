e7299ffc0a99c83b0e83c4148c9f2b608b99bfec
maratkanov-a <a.s.maratkanov@gmail.com>
2016-04-05 09:41:51 +0300

    added news in admin page

--- clinic_app/models.py
+++ clinic_app/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -7,5 +8,8 @@ class News(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=500)
     link = models.CharField(max_length=50)
-    date_pablished = models.DateTimeField(default=datetime.datetime.now())
+    date_published = models.DateTimeField(default=timezone.now())
     image = models.ImageField()
+
+    def __unicode__(self):
+        return self.title
